16a1c7233b47d20d6fe0698b5f2af997f1cc3a98
Wionek <krystianjonca43@gmail.com>
2022-05-13 15:04:36 +0200

    Added colormode change

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import Layout from './components/Layout';
 import Home from './routes/Home';
 import Coins from './routes/Coins';
 import News from './routes/News';
 import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+import { useSelector } from 'react-redux';
 
 const App = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
+  const colorMode = useSelector((state) => state.colorMode.value);
 
   useEffect(() => {
     if (pathname === '/') navigate('/home');
   }, []);
 
+  const theme = useMemo(
+    () => createTheme({ palette: { mode: colorMode } }),
+    [colorMode]
+  );
+
   return (
     <>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={theme}>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="/home" element={<Home />} />

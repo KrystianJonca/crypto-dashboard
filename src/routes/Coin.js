6d7409de7b723fb35b7184178edf5b123fdf9895
Wionek <krystianjonca43@gmail.com>
2022-05-15 13:21:29 +0200

    Added coin page

--- src/routes/Coin.js
+++ src/routes/Coin.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Typography } from '@mui/material';
-import { Loader } from '../components';
+import { Loader, CoinDetail } from '../components';
 import { useParams } from 'react-router-dom';
 import { useGetCoinQuery } from '../services/coinsApi';
 
 const Coin = () => {
   const { id } = useParams();
-  const { data: coin, error, isLoading } = useGetCoinQuery(id);
+  const { data: coinData, error, isLoading } = useGetCoinQuery(id);
+
+  const coin = useMemo(
+    () => (isLoading ? null : coinData.data.coin),
+    [coinData, isLoading]
+  );
 
   if (error) return <Typography variant="h4">{error}</Typography>;
-  // TODO
+
   return (
     <>
-      {isLoading ? <Loader /> : <Typography>{coin.data.coin.name}</Typography>}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <CoinDetail
+          id={coin.uuid}
+          name={coin.name}
+          price={coin.price}
+          symbol={coin.symbol}
+          icon={coin.iconUrl}
+          desc={coin.description}
+          volume={coin['24hVolume']}
+          supply={coin.supply.total}
+          ath={coin.allTimeHigh.price}
+          marketCap={coin.marketCap}
+        />
+      )}
     </>
   );
 };
